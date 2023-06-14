67f49fc8dc4eec6e44fc9b996e2602192e142a72
ubyhzargam <ubyhzargam@gmail.com>
2023-06-14 18:43:43 +0530

    Update main.cpp

--- ncr_using_recursion/main.cpp
+++ ncr_using_recursion/main.cpp
@@ -6,9 +6,20 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int C(int n, int r)
+{
+    if(r==0||r==n)
+        return 1;
+    else
+        return C(n-1,r-1)+C(n- 1,r);
+}
+int main()
+{
+    int n,r;
+    cout<<"Enter n and r respectively to find ncr : ";
+    cin>>n;
+    cin>>r;
+    cout<<"The result is = "<<C(n,r)<<endl;
     return 0;
 }
